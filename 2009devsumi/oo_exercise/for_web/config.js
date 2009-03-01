42716d8dcdd8c1233808266d42d9054510c60316
yoji.kanno <yoji.kanno@a0babc10-e826-0410-9ddb-bf8b29f1f16a>
2009-03-01 09:25:31 +0000

    いったんコミット

--- 2009devsumi/oo_exercise/for_web/config.js
+++ 2009devsumi/oo_exercise/for_web/config.js
@@ -5,8 +5,7 @@ config = {
  
  /*内容*/
  number: '[2008 年 3 月号]',
- title :'Developer Summit 参加報告',
+ title :'Developers Summit 参加報告',
  subtitle :'オブジェクト指向エクササイズのススメ',
- desc : 'XXXXXXXXXXXXXXXXXXXXXXXXX<br>' ,
- url: 'http://www.ogis-ri.co.jp/otc/hiroba/others/OORing/interview40.html',
+ url: 'xxx.xxx.xxx',
 }
